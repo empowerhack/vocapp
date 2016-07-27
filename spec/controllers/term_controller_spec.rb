d5c099575f0421dd1ada716d23fed79b32ae822d
Rhiannon Lolley Neville <rrlolleyneville@gmail.com>
2016-07-27 16:49:37 +0100

    deleted empty test

--- spec/controllers/term_controller_spec.rb
+++ spec/controllers/term_controller_spec.rb
@@ -4,14 +4,10 @@ describe TermsController, type: :controller do
   let!(:term) {FactoryGirl.create(:term)}
 
   describe "GET term" do
-      it "gets term by id " do
-        term = FactoryGirl.create(:term)
-        get :show, id: term
-        assigns(:term).should eq(term)
-      end
-
-      it "" do
-
-      end
+    it "gets term by id " do
+      term = FactoryGirl.create(:term)
+      get :show, id: term
+      assigns(:term).should eq(term)
     end
+  end
 end
