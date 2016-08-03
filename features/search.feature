73d717ac38b5da82fce07d7057f0ff689c3154a3
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-08-03 13:08:01 +0100

    Added steps for searching a term

---
+++ features/search.feature
@@ -0,0 +1,10 @@
+Feature: Search
+
+  Scenario: Searching for a term
+    Given there's a search button
+    When I type 'algorithm' into the search field and click search
+    Then I should see the Terms page
+    And I should see a table
+    And in the first cell of the table I should see 'algorithm' and 'Information technology'
+
+    
---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,19 @@
+Given(/^there's a search button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I type 'algorithm' into the search field and click search$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the Terms page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a table$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^in the first cell of the table I should see 'algorithm' and 'Information technology'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
