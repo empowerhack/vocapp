d5d606a2416d7b152acb10c3788ec587af8cab60
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-08-16 14:12:16 +0100

    fixed the factory girl and test now passing

--- spec/factories/term_factory.rb
+++ spec/factories/term_factory.rb
@@ -3,12 +3,6 @@ include ActionDispatch::TestProcess
 FactoryGirl.define do
   factory :term do
     term_en { Faker::Hipster.sentence }
-    term_ar { Faker::Hipster.sentence }
-    ac_field_en { Faker::Hipster.sentence }
-    ac_field_ar { Faker::Hipster.sentence }
-    definition_en { Faker::Hipster.sentence }
-    definition_ar { Faker::Hipster.sentence }
     context_en { Faker::Hipster.sentence }
-    context_ar { Faker::Hipster.sentence }
   end
 end
