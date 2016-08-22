254c0901fc92c07c7a929688c2748cc855a6793a
Paul <paulalexrees@gmail.com>
2016-08-22 13:57:34 +0100

    adds answers to seeding and removes erroneous field

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ terms = []
 # Runs through each row and stores field and term+field data in arrays
 CSV.foreach(csv_text, headers: true) do |row|
   fields << row[2].capitalize
-  terms << [row[0].downcase, row[2].capitalize]
+  terms << [row[0].downcase, row[2].capitalize, row[4]]
 end
 
 # Creates a user so the DB has a user_id of 1
@@ -25,7 +25,9 @@ User.create!(email:"p@p.com", password:"password")
 fields.uniq.each { |f| Field.create!(name: f) }
 
 # Assigns the correct field_id to each term, plus user_id of 1
+# Creates answers for each term
 terms.each.with_index do |t, i|
   field_id = Field.where(name: t[1])[0].id
   Term.create!(term_en: t[0], user_id: 1, field_id: field_id)
+  Answer.create!(definition: t[2], user_id: 1, term_id: i+1)
 end
