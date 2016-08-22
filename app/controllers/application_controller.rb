ca0a22862e593f2b165141f94583bd4ffe860cc0
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-08-22 14:00:22 +0100

    added return to previous location after sign_in methods to applicatioin controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :null_session
 
+  after_filter :store_location
+
   def index
     @fields = Field.all 
   end
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
 end
