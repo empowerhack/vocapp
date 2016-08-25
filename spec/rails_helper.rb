c01cc3436883ae0ab53aa6886407668330a06290
eripheebs <erikapheby@gmail.com>
2016-08-25 11:58:08 +0100

    fixing-tests models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,9 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'capybara/rails'
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
 
 ActiveRecord::Migration.maintain_test_schema!
 
