fa4390df490cc2e72a3401202af4ad104122b429
eripheebs <erikapheby@gmail.com>
2016-08-25 16:08:00 +0100

    Merging tests

--- spec/controllers/term_controller_spec.rb
+++ spec/controllers/term_controller_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 describe TermsController, type: :controller do
-  let!(:term) {FactoryGirl.create(:term)}
+  let!(:term) { FactoryGirl.create(:term) }
 
   describe "GET term" do
     it "gets term by id " do
-      term = FactoryGirl.create(:term)
       get :show, id: term
       assigns(:term).should eq(term)
     end
