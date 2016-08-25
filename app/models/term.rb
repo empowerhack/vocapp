c0ad6975c29d0159f5bb41d3d70d832a13d95c30
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-08-25 15:16:32 +0100

    DB and FE validations for term_en being present in terms table

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,6 +1,9 @@
 class Term < ActiveRecord::Base
   validates :term_en, uniqueness: { scope: :field, message: '"%{value}" exists for this academic field already.' }
+  validates :term_en, presence: true
+
   belongs_to :user
   belongs_to :field
+
   has_many :answers
 end
