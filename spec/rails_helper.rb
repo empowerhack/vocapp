0d00b23d0517f70cf0337e9f84b0e1fe45541f2a
eripheebs <erikapheby@gmail.com>
2016-08-30 15:45:23 +0100

    Seeding for rspec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+    load "#{Rails.root}/db/seeds.rb"
   end
 
   config.before(:each) do
