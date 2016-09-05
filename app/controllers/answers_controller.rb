2cef2c6e84a397a29351f486b9826e41c8070626
Paul <paulalexrees@gmail.com>
2016-09-05 19:03:40 +0100

    can edit an answer. link only shows if you made it. needs constraints though

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!, :except => [:index, :show]
 
   def create
     @term = Term.find(params[:term_id])
@@ -6,6 +7,20 @@ class AnswersController < ApplicationController
     @answer.user_id = current_user.id
     @answer.save
     redirect_to "/terms/#{@term.id}"
+  end
+
+  def edit
+    @answer = Answer.find(params[:id])
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    @term = Term.find(@answer.term_id)
+    if @answer.update(answer_params)
+      redirect_to "/terms/#{@term.id}"
+    else
+      render 'edit'
+    end
   end
 
   private
