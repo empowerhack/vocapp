2ffe7ea95fdb6b58b76ac95e69c1e1054d3a611e
eripheebs <erikapheby@gmail.com>
2016-09-06 18:19:34 +0100

    Changed repeated method name and added seeding instructions for tests to readme

--- app/helpers/flags_helper.rb
+++ app/helpers/flags_helper.rb
@@ -3,7 +3,7 @@ module FlagsHelper
     params.require('flag').permit(:answer_id)
   end
 
-  def find_answer(flag)
-    return Answer.find(flag.answer_id)
+  def find_answer_from_flag(flag)
+    return find_answer(flag.answer_id)
   end
 end
