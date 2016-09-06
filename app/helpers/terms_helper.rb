a951ad53836878ae5f4bb74428867ef305f20897
eripheebs <erikapheby@gmail.com>
2016-09-06 16:19:35 +0100

    refactored terms controller & helper methods

--- app/helpers/terms_helper.rb
+++ app/helpers/terms_helper.rb
@@ -1,5 +1,4 @@
 module TermsHelper
-
   def term_params
     if params['term']
       params['term'].permit(:term_en, :field_id, :user_id)
@@ -25,5 +24,27 @@ module TermsHelper
 
   def filtered_results(field_id)
     @terms.where(field_id: field_id)
+  end
+
+  def field_selected?(field_id)
+    field_id && field_id != ""
+  end
+
+  def notify_and_redirect(term_en)
+    flash[:notice] = "Your term \"#{term_en}\" has been submitted."
+    redirect_to '/terms'
+  end
+
+  def alert_and_redirect(term)
+    flash[:alert] = term.errors.empty? ? generic_error : specific_error(term)
+    redirect_to '/terms/new'
+  end
+
+  def generic_error
+    return "Sorry, there was a problem submitting your new term."
+  end
+
+  def specific_error(term)
+    term.errors.full_messages.to_sentence
   end
 end
