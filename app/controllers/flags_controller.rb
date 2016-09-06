885ebab97d7054cd766ebcce1ecd3a8c2eb30afe
Erika Pheby <erikapheby@gmail.com>
2016-09-06 17:34:12 +0100

    admin merge conflict resolved

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -3,7 +3,7 @@ class FlagsController < ApplicationController
 
   def create
     answer = find_answer(params[:answer_id])
-    if current_user.has_flagged? answer
+    if current_user.has_flagged? answer or current_user.admin?
       flash[:notice] = 'You cannot flag more than once'
       refresh_term_page(params[:term_id])
     else
