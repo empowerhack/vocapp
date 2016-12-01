3bd26b03a72c39ed2b593a5af9daf69d114029db
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-12-01 15:02:05 +0000

    added url_reference to the answer form and display on the terms show page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -33,6 +33,6 @@ class AnswersController < ApplicationController
 
   private
     def answer_params
-      params.require(:answer).permit(:definition, :upvotes, :downvotes, :term_ar_match, :context_en, :context_ar)
+      params.require(:answer).permit(:definition, :upvotes, :downvotes, :term_ar_match, :context_en, :context_ar, :ref_url)
     end
 end
